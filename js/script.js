5a32aeec2fffc521c3ed5b513f7c63f995e43df7
Quetal13 <nelsonebarreto13@gmail.com>
2024-01-12 13:57:51 -0400

    User Interface
    I'm start adding an user interface to my rock, paper, scissors.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const allButtons = document.querySelectorAll('.button-container')
+const restartButton = document.querySelector('#restart');
+const round = document.querySelector('.round-counter');
+const roundResult = document.querySelector('.round');
+const computerPoints = document.querySelector('.computer-score');
+const playerPoints = document.querySelector('.player-score');
+const winner = document.querySelector('.winner');
 
-//Add a conditional that check the player's choice
-//Return the player's choice
-
-let roundResult;
 let playerPointsCounter = 0;
 let computerPointsCounter = 0;
+let roundCounter = 0;
 
 function getComputerChoice() {
     const choiceArray = [
@@ -21,55 +24,71 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     //this conditionals check the round results for edit the roundResult var
     if (playerSelection === computerSelection) {
-        roundResult ='It\'s a tie';
+        roundResult.textContent ='Result: It\'s a tie';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        roundResult = 'You lose! Paper beats Rock';
+        roundResult.textContent = 'Result: You lose! Paper beats Rock';
         computerPointsCounter++
+        computerPoints.textContent = 'Computer score: ' + computerPointsCounter;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        roundResult ="You win! Paper beats Rock";
+        roundResult.textContent ="Result: You win! Paper beats Rock";
         playerPointsCounter++
+        playerPoints.textContent = 'Player score: ' + playerPointsCounter;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        roundResult = "You win! Rock beats Scissors";
+        roundResult.textContent = "Result: You win! Rock beats Scissors";
         playerPointsCounter++
+        playerPoints.textContent = 'Player score: ' + playerPointsCounter;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        roundResult = "You lose! Rock beats Scissors";
+        roundResult.textContent = "Result: You lose! Rock beats Scissors";
         computerPointsCounter++
+        computerPoints.textContent = 'Computer score: ' + computerPointsCounter;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        roundResult = "You lose! Scissors beats Paper";
+        roundResult.textContent = "Result: You lose! Scissors beats Paper";
         computerPointsCounter++
+        computerPoints.textContent = 'Computer score: ' + computerPointsCounter;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        roundResult ="You win! Scissors beats Paper";
+        roundResult.textContent ="Result: You win! Scissors beats Paper";
         playerPointsCounter++
+        playerPoints.textContent = 'Player score: ' + playerPointsCounter;
     }
 
 }
 
-
-
-//game();
-
-/*function game() { //This function is responsible for to play a best-of-five game and print in the console the result of the round and the result of the game
-    for (let i = 0; i < 5; i++) {
+function game() { //This function is responsible for to play a best-of-five game and print in the console the result of the round and the result of the game
         if (playerPointsCounter === 3) {
-            console.log('Congratulations! You win!');
-            break;
+            winner.textContent = 'Winner: Congratulations! You win!';
+            roundCounter = 5;
         } else if (computerPointsCounter === 3) {
-            console.log('I\'m sorry, you loose :(');
-            break;
-        } else if (i === 4 && playerPointsCounter > computerPointsCounter) {
-            console.log('Congratulations! You win!');
-        } else if (i === 4 && playerPointsCounter < computerPointsCounter) {
-            console.log('I\'m sorry, you loose :(');
-        } else if (i === 4 && playerPointsCounter === computerPointsCounter) {
-            console.log('Nobody wins, it\'s a tie!');
+            winner.textContent = 'Winner: I\'m sorry, you loose :(';
+            roundCounter = 5;
+        } else if (roundCounter === 5 && playerPointsCounter > computerPointsCounter) {
+            winner.textContent = 'Winner: Congratulations! You win!';
+        } else if (roundCounter === 5 && playerPointsCounter < computerPointsCounter) {
+            winner.textContent = 'Winner: I\'m sorry, you loose :(';
+        } else if (roundCounter === 5 && playerPointsCounter === computerPointsCounter) {
+            winner.textContent = 'Winner: Nobody wins, it\'s a tie!';
         }
-    }
-} */
+}
 
 allButtons.forEach((button) => {
 button.addEventListener('click', function(ev) {
     const computerSelection = getComputerChoice();
-    playRound(ev.target.value, computerSelection);
-    console.log(roundResult);
+    if (roundCounter < 5) {
+        playRound(ev.target.value, computerSelection);
+        roundCounter++
+        round.textContent = `Rounds: ` + roundCounter;
+        game();
+    }
 })
+})
+
+restartButton.addEventListener('click', function() {
+  roundCounter = 0;
+  playerPointsCounter = 0;
+  computerPointsCounter = 0;
+
+  round.textContent = `Rounds: ` + roundCounter;
+  playerPoints.textContent = 'Player score: ' + playerPointsCounter;
+  computerPoints.textContent = 'Computer score: ' + computerPointsCounter;
+  roundResult.textContent = "Result:";
+  winner.textContent = 'Winner:';
 })
